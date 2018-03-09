b1159f09782441dc5b97a28b3672b5a6c5a2d404
antonstenaxel <antkarls@student.chalmers.se>
2018-03-09 16:01:48 +0100

    Changes

--- Lab_4/findDisparity.m
+++ Lab_4/findDisparity.m
@@ -2,20 +2,20 @@ function d = findDisparity(window, panel,column)
 
 nCols = size(panel,2);
 offset = floor(size(window,1)/2);
-fVec = zeros(1,nCols);
 
+index = -1; 
+diff_min = inf; 
 for col_2 = offset+1: nCols-offset
   
   testWindow = panel(:,col_2-offset:col_2+offset);
   %difference = sum(sum((window - testWindow).^2));
   difference = sum(sum(abs(window - testWindow)));
-  %difference = crosscorr(reshape(testWindow,1,[]),reshape(window,1,[]));
-  f =1/difference; 
   
-  fVec(col_2)=f;
+  if(difference < diffmin)
+    index = col_2;
+  end
+  
 end
-
-[~ ,index] = max(fVec);
 
 d = column - index; 
 
