4087447412537198c77d711b9f097bcab189f271
antonstenaxel <antkarls@student.chalmers.se>
2018-03-10 08:54:02 +0100

    Changes

--- Lab_4/findDisparityMap.m
+++ Lab_4/findDisparityMap.m
@@ -1,28 +1,30 @@
-function D = findDisparityMap(I1, I2,windowSize)
+function D = findDisparityMap(I1, I2,windowSize,range)
 %Windowsize have to be uneven
 
 nRows = size(I1,1);
 nCols = size(I1,2);
 
+windowOffset = floor(windowSize/2);
+panelsize = windowSize + 2*range; 
 
 D = zeros(nRows,nCols);
-offset = floor(windowSize/2);
 tic
 %For each pixel
 told = toc;
 
-for row = offset+1:nRows - offset
-  
-  panel = I2(row-offset:row+offset,:);
+for row = windowOffset+1:nRows - windowOffset
   if(mod(row,10) == 0 )
     tnew = toc; 
-    fprintf("%.2f  completed \nEstimated time left: %.2f mins \n",row/(nRows-offset)*100,(tnew-told)/10*(nRows-row)/60);
+    fprintf("%.2f  completed \nEstimated time left: %.2f mins \n",row/(nRows)*100,(tnew-told)/10*(nRows-row)/60);
     told =tnew;
   end
   
-  for col = offset+1:nCols - offset
-    
-    window = I1(row-offset:row+offset,col-offset:col+offset);
+  for col = floor(panelsize/2)+1:nCols - floor(panelsize/2)
+    panel = I2(row-windowOffset:row+windowOffset,...
+               col-floor(panelsize/2):col+floor(panelsize/2));
+             
+    window = I1(row-windowOffset:row+windowOffset,...
+                col-windowOffset:col+windowOffset);
     
     disparity = findDisparity(window, panel,col);
     
--- Lab_4/main.m
+++ Lab_4/main.m
@@ -1,9 +1,10 @@
 I1 = rgb2gray(imread('Images/tsukuba1.png'));
 I2 = rgb2gray(imread('Images/tsukuba2.png'));
 windowSize=5;
+range = 30; 
 
 
-disparityMap = findDisparityMap(I1,I2,windowSize); 
+disparityMap = findDisparityMap(I1,I2,windowSize,range); 
 %%
 subplot(1,2,1)
 imshow(abs(disparityMap)./max(max(abs(disparityMap)))); 
