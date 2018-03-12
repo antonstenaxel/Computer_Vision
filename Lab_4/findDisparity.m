8361293af62c979776b5b78016868381beade35c
antonstenaxel <antkarls@student.chalmers.se>
2018-03-12 12:48:23 +0100

    Changes

--- Lab_4/findDisparity.m
+++ Lab_4/findDisparity.m
@@ -9,8 +9,8 @@ maxFitness = 0;
 for col = offset+1:nCols-offset
   
   testWindow = panel(:,col-offset:col+offset);
-  difference = sum(sum((window - testWindow).^2));
-  %difference = sum(sum(abs(window - testWindow)));
+  %difference = sum(sum((window - testWindow).^2));
+  difference = sum(sum(abs(window - testWindow)));
   fitness = 1/difference; 
   if(fitness > maxFitness)
     matchingColumn = col; % OBS matching column is in reference of panel
--- Lab_4/main.m
+++ Lab_4/main.m
@@ -3,11 +3,12 @@ I2 = rgb2gray(imread('Images/venus/im6.ppm'));
 
 %%
 
-windowSize=[15,15]; %[rows,columns]
+windowSize=[25,25]; %[rows,columns]
 disparityRange = [-20,20]; 
 
 
 disparityMap = findDisparityMap(I1,I2,windowSize,disparityRange); 
+
 
 imshow(disparityMap,disparityRange)
 
